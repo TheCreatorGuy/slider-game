912d9053a8598ab0bbf2525ff4d14b5495d13959
TheCreatorGuy <timothy.swagmoney.johnson@gmail.com>
2018-09-28 13:41:32 -0400

    commented file

--- slider_level_tester.py
+++ slider_level_tester.py
@@ -1,5 +1,5 @@
 """
-Filename: slider_solver.py
+Filename: slider_level_tester.py
 Interpreter: 2.7
 Author: Tim Johnson
 Description: A project designed to find the best solution to every level of my slider game.
@@ -7,6 +7,10 @@ Description: A project designed to find the best solution to every level of my s
 import slidergame, sys
 
 def main():
+    """
+    Starts the game at the given level
+    :return: None
+    """
     if len(sys.argv) != 2:
         slidergame.currentLevel = int(raw_input("What level do you want to test? "))
     else:
